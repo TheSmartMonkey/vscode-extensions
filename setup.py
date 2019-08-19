55b12ecaeaba51a6e98d339c4b5bf8106c036833
laurent.vandelle <laurent.vandelle@usharesoft.com>
2019-08-19 10:58:15 +0200

    add setup.py automate script

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python vscode.py -k /home/developer/.config/Code/User/")
+os.system("python vscode.py -s /home/developer/.config/Code/User/")
+os.system("python vscode.py --install")
+
+print("Setup vscode COMPLETED")
