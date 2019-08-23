37cde38b4de34df189b22ac0fb31f06cdd8c86cd
laurentvandelle <31937978+TheSmartMonkey@users.noreply.github.com>
2019-08-23 12:10:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
+# Deploys the project using the serverless framework to AWS lambda
+os.system("cd ~/WKS/src/gitlab.com/moonshotlabs/mylegal")
+os.system("make clean")
+os.system("make deploy")
+
+# Setup my Visual Studio Code
+os.system("cd ~/vscode-extensions")
 os.system("python vscode.py -k /home/developer/.config/Code/User/")
 os.system("python vscode.py -s /home/developer/.config/Code/User/")
+print("\n")
 os.system("python vscode.py --install")
 
-print("Setup vscode COMPLETED")
+# Go back to main project
+os.system("cd ~/WKS/src/gitlab.com/moonshotlabs/mylegal/ui")
+
+print("\nSetup vscode COMPLETED")
